02df9565cf91a88c8c7d5f0643e3df8e66a0417f
Iain Maryanow <imaryanow@yahoo.com>
2015-07-09 15:21:57 -0700

    Add multinomial.

--- logreg.js
+++ logreg.js
@@ -2,6 +2,7 @@ function logisticRegression() {
 	var n,
 		dims,
 		beta,
+		cl,
 		math = mathematics();
 
 	my.train = function(data, output, options) {
@@ -9,12 +10,62 @@ function logisticRegression() {
 			throw new Error("Number of datapoints and number of outputs must match.");
 		}
 
+		options = options || {};
+
+		var classes = [];
+		for (var i in output) {
+			if (classes.indexOf(output[i][0]) === -1) {
+				classes.push(output[i][0]);
+			}
+		}
+
+		cl = classes.length;
+
+		if (cl === 1) {
+			throw new Error("Output must consist of more than one class.");
+		}
+		else if (cl === 2) {
+			binaryLogReg(data, output, options);
+		}
+		else {
+			multinomialLogReg(data, output, options);
+		}
+
+		return my;
+	}
+
+	my.predict = function(point) {
+		point.unshift(1);
+
+		if (cl > 2) {
+			var current = -1,
+				max = 0;
+
+			for (var c = 0; c < cl; c++) {
+				if (math.sigmoid(math.dotProduct(beta[c], point)) > max) {
+					current = c;
+					max = math.sigmoid(math.dotProduct(beta[c], point));
+				}
+			}
+
+			return [current, max];
+		}
+		else {
+			return math.sigmoid(math.dotProduct(beta, point));
+		}
+	}
+
+	my.beta = function() {
+		return beta;
+	}
+
+	function binaryLogReg(data, output, options) {
 		var options = options || {},
 			maxIter = options["maxIter"] || 500,
 			stepSize = options["stepSize"] || .25,
 			sum;
 
-			n = Math.max(data.length, 500),
+			n = Math.min(data.length, 500),
 			dims = data[0].length,
 			beta = math.zeroVector(dims + 1);
 
@@ -33,33 +84,56 @@ function logisticRegression() {
 				sum = math.addVectors(sum, mult);
 			}
 
-			// If the gradient is ever 0, then the algorithm has converged
-			if (math.vectorsEqual(sum, math.zeroVector(dims + 1))) {
-				break;
-			}
-
-			sum = math.vectorMultiplyScalar(sum, 1/n);
-			sum = math.vectorMultiplyScalar(sum, stepSize);
-
+			sum = math.vectorMultiplyScalar(sum, stepSize/n);
 			beta = math.addVectors(beta, sum);
+		}
+	}
 
-			console.log("Iter " + i);
+	function multinomialLogReg(data, output, options) {
+		n = Math.min(data.length, 500),
+		dims = data[0].length,
+		beta = math.zeroMatrix(cl, dims + 1);
+
+		var options = options || {},
+			maxIter = options["maxIter"] || 500,
+			stepSize = options["stepSize"] || .25,
+			sum,
+			denom,
+			probs = math.zeroMatrix(n, cl);
+
+		// Add bias to each datapoint
+		for (var i = 0; i < n; i++) {
+			data[i].unshift(1);
 		}
 
-		return my;
-	}
+		for (var i = 0; i < maxIter; i++) {
+			for (var t = 0; t < n; t++) {
+				denom = 0;
+				for (var c = 0; c < cl; c++) {
+					denom += Math.exp(math.dotProduct(beta[c], data[t]));
+				}
 
-	my.predict = function(point) {
-		point.unshift(1);
-		return math.sigmoid(math.dotProduct(beta, point));
-	}
+				for (var c = 0; c < cl; c++) {
+					probs[t][c] = Math.exp(math.dotProduct(beta[c], data[t])) / denom;
+				}
+			}
 
-	my.score = function() {
+			for (var c = 0; c < cl; c++) {
+				var gradient = math.zeroVector(dims + 1);
 
-	}
+				for (var t = 0; t < n; t++) {
+					if (output[t] == c) {
+						gradient = math.addVectors(gradient, math.vectorMultiplyScalar(data[t], 1 - probs[t][c]));
+					}
+					else {
+						gradient = math.addVectors(gradient, math.vectorMultiplyScalar(data[t], 0 - probs[t][c]));
+					}
+				}
 
-	my.beta = function() {
-		return beta;
+				gradient = math.vectorMultiplyScalar(gradient, stepSize/n);
+				beta[c] = math.addVectors(beta[c], gradient);
+			}
+		}
 	}
 
 	function my(){}
